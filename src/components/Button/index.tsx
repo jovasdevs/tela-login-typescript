2497f25feb1cec70aefac64ce9dcf457034acb4f
jovasdevs <jovasdevs@gmail.com>
2022-12-20 15:02:50 -0300

    Implementado a função de desabilitar o botão.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
+const Button = ({ title, onClick,  }: IButtonProps): JSX.Element => {
   return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
 };
 
